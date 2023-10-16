a75da4298fcfb2edc3f8ce041374d092931bd4c1
stampFabian <fabian.trenkwalder@hak-kitz.ac.at>
2023-10-16 10:47:47 +0200

    fix: small accManager fixes

--- AutoLogin/AccountManager.Designer.cs
+++ AutoLogin/AccountManager.Designer.cs
@@ -37,23 +37,34 @@ namespace AutoLogin
             this.dataGrid1 = new System.Windows.Forms.DataGrid();
             this.accNameLbl = new System.Windows.Forms.Label();
             this.btnRefresh = new System.Windows.Forms.Button();
+            this.tbType = new System.Windows.Forms.TextBox();
+            this.tbInfo = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
             this.SuspendLayout();
             // 
             // saveBtn
             // 
-            this.saveBtn.Location = new System.Drawing.Point(551, 385);
+            this.saveBtn.Location = new System.Drawing.Point(403, 385);
             this.saveBtn.Name = "saveBtn";
-            this.saveBtn.Size = new System.Drawing.Size(228, 53);
+            this.saveBtn.Size = new System.Drawing.Size(191, 53);
             this.saveBtn.TabIndex = 0;
             this.saveBtn.Text = "save";
             this.saveBtn.UseVisualStyleBackColor = true;
             // 
             // undoBtn
             // 
-            this.undoBtn.Location = new System.Drawing.Point(289, 385);
+            this.undoBtn.Location = new System.Drawing.Point(9, 385);
             this.undoBtn.Name = "undoBtn";
-            this.undoBtn.Size = new System.Drawing.Size(228, 53);
+            this.undoBtn.Size = new System.Drawing.Size(191, 53);
             this.undoBtn.TabIndex = 1;
             this.undoBtn.Text = "undo changes";
             this.undoBtn.UseVisualStyleBackColor = true;
@@ -61,9 +72,9 @@ namespace AutoLogin
             // 
             // cancelBtn
             // 
-            this.cancelBtn.Location = new System.Drawing.Point(24, 385);
+            this.cancelBtn.Location = new System.Drawing.Point(206, 385);
             this.cancelBtn.Name = "cancelBtn";
-            this.cancelBtn.Size = new System.Drawing.Size(228, 53);
+            this.cancelBtn.Size = new System.Drawing.Size(191, 53);
             this.cancelBtn.TabIndex = 2;
             this.cancelBtn.Text = "cancel";
             this.cancelBtn.UseVisualStyleBackColor = true;
@@ -73,9 +84,9 @@ namespace AutoLogin
             // 
             this.dataGrid1.DataMember = "";
             this.dataGrid1.HeaderForeColor = System.Drawing.SystemColors.ControlText;
-            this.dataGrid1.Location = new System.Drawing.Point(113, 84);
+            this.dataGrid1.Location = new System.Drawing.Point(24, 83);
             this.dataGrid1.Name = "dataGrid1";
-            this.dataGrid1.Size = new System.Drawing.Size(577, 268);
+            this.dataGrid1.Size = new System.Drawing.Size(457, 268);
             this.dataGrid1.TabIndex = 3;
             // 
             // accNameLbl
@@ -85,35 +96,152 @@ namespace AutoLogin
             this.accNameLbl.Name = "accNameLbl";
             this.accNameLbl.Size = new System.Drawing.Size(154, 43);
             this.accNameLbl.TabIndex = 4;
-            this.accNameLbl.Text = "see btn name";
             // 
             // btnRefresh
             // 
             this.btnRefresh.BackgroundImage = global::AutoLogin.Properties.Resources.refresh;
             this.btnRefresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnRefresh.Location = new System.Drawing.Point(683, 28);
+            this.btnRefresh.Location = new System.Drawing.Point(709, 28);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(41, 43);
             this.btnRefresh.TabIndex = 5;
             this.btnRefresh.UseVisualStyleBackColor = true;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
+            // tbType
+            // 
+            this.tbType.Location = new System.Drawing.Point(500, 96);
+            this.tbType.Name = "tbType";
+            this.tbType.Size = new System.Drawing.Size(178, 22);
+            this.tbType.TabIndex = 6;
+            // 
+            // tbInfo
+            // 
+            this.tbInfo.Location = new System.Drawing.Point(500, 156);
+            this.tbInfo.Name = "tbInfo";
+            this.tbInfo.Size = new System.Drawing.Size(178, 22);
+            this.tbInfo.TabIndex = 7;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(500, 216);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(178, 22);
+            this.tbEmail.TabIndex = 8;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(500, 276);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(178, 22);
+            this.tbUsername.TabIndex = 9;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(500, 335);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(178, 22);
+            this.tbPassword.TabIndex = 10;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(600, 385);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(191, 53);
+            this.addBtn.TabIndex = 11;
+            this.addBtn.Text = "add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Sitka Text", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(500, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 32);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "type";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Sitka Text", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(500, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 32);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "info";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Sitka Text", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(500, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 32);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "email";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Sitka Text", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(500, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 32);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "username";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Sitka Text", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(500, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 32);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "password";
+            // 
             // AccountManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbInfo);
+            this.Controls.Add(this.tbType);
             this.Controls.Add(this.btnRefresh);
             this.Controls.Add(this.accNameLbl);
             this.Controls.Add(this.dataGrid1);
             this.Controls.Add(this.cancelBtn);
             this.Controls.Add(this.undoBtn);
             this.Controls.Add(this.saveBtn);
+            this.Location = new System.Drawing.Point(15, 15);
             this.Name = "AccountManager";
-            this.Text = "AccountManager";
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TextBox tbType;
+        private System.Windows.Forms.TextBox tbInfo;
+        private System.Windows.Forms.TextBox tbEmail;
 
         private System.Windows.Forms.Button btnRefresh;
 
